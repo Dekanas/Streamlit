6d5080125f428e281333913340ce72dafd2f9ee9
Dekanas <44368441+Dekanas@users.noreply.github.com>
2022-10-11 20:08:55 +0300

    Update ev_stats.py

--- ev_stats.py
+++ ev_stats.py
@@ -24,8 +24,7 @@ product = st.sidebar.selectbox(
               '>50000'
             ])
 if product == '<50000':
-  sns.set(style="ticks", context="talk")
-  plt.style.use('ggplot')
+  sns.set_theme(style="whitegrid")
   f, ax = plt.subplots(figsize=(10, 15))
 
   sns.set_color_codes("pastel")
@@ -39,8 +38,7 @@ if product == '<50000':
   sns.despine(left=True, bottom=True)
   st.pyplot(f)
 else:
-  sns.set(style="ticks", context="talk")
-  plt.style.use('ggplot')
+  sns.set_theme(style="whitegrid")
   f, ax = plt.subplots(figsize=(10, 15))
 
   sns.set_color_codes("pastel")
